dff2da8c816f2052ba0b33da7d75c88e5091c78b
priyaprecious <priyanka.datti@gmail.com>
2022-01-26 13:48:15 -0500

    props and state in the appointment form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,47 +1,74 @@
 import React, { useState } from 'react';
-import Button from 'components/Button';
-import InterviewerList from 'components/InterviewerList';
+import Button from '../Button';
+import InterviewerList from '../InterviewerList';
 
-export default function Form({ student: inputStudent, interviewer: inputInterviewer, interviewers, onSave, onCancel }) {
-  const [student, setStudent] = useState(inputStudent || '');
-  const [interviewer, setInterviewer] = useState(inputInterviewer || null);
+
+const Form = (props) => {
+  const [name, setName] = useState(props.name || '');
+  const [error, setError] = useState('');
+  const [noInterviewerError, setNoInterviewerError] = useState('');
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
   const reset = () => {
-    setStudent('');
+    setName('');
     setInterviewer(null);
   };
 
   const cancel = () => {
     reset();
-    onCancel();
+    props.onCancel();
+  };
+
+  const validate = () => {
+    // check student name is inputted
+    if (name) {
+      setError('');
+    } else {
+      setError('Student name cannot be blank');
+      return;
+    }
+
+    props.onSave(name, interviewer);
   };
 
   return (
-    <main className="appointment__card appointment__card--create">
-      <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
+    <main className='appointment__card appointment__card--create'>
+      <section className='appointment__card-left'>
+        <form autoComplete='off' onSubmit={(event) => event.preventDefault()}>
           <input
-            className="appointment__create-input text--semi-bold"
-            name="name"
-            type="text"
-            placeholder="Enter Student Name"
-            value={student}
-            onChange={(e) => setStudent(e.target.value)}
+            className='appointment__create-input text--semi-bold'
+            name='name'
+            type='text'
+            placeholder='Enter Student Name'
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            data-testid='student-name-input'
           />
+          <section className='appointment__validation'>
+            {error}
+          </section>
+          <InterviewerList
+            interviewers={props.interviewers}
+            value={interviewer}
+            onChange={setInterviewer}
+          />
+          <section className='appointment__validation'>
+            {noInterviewerError}
+          </section>
         </form>
-        <InterviewerList
-          interviewers={interviewers}
-          setInterviewer={setInterviewer}
-          interviewer={interviewer}
-        />
       </section>
-      <section className="appointment__card-right">
-        <section className="appointment__actions">
-          <Button danger onClick={() => cancel()}>Cancel</Button>
-          <Button confirm onClick={() => onSave(student, interviewer)}>Save</Button>
+      <section className='appointment__card-right'>
+        <section className='appointment__actions'>
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={validate}>
+            Save
+          </Button>
         </section>
       </section>
     </main>
-
   );
-}
+};
+
+export default Form;
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,21 +2,25 @@ import React from 'react';
 import 'components/InterviewerList.scss';
 import InterviewerListItem from './InterviewerListItem';
 
-export default function InterviewerList({ interviewers, value, onChange }) {
-    return (
-        <section className="interviewers">
-            <h4 className="interviewers__header text--light">Interviewer</h4>
-            <ul className="interviewers__list">
-                {interviewers && interviewers.map((interviewer) => (
-                    <InterviewerListItem
-                        key={interviewer.id}
-                        name={interviewer.name}
-                        avatar={interviewer.avatar}
-                        selected={interviewer.id === value}
-                        setInterviewer={(event) => onChange(interviewer.id)}
-                    />
-                ))}
-            </ul>
-        </section>
-    );
+
+const InterviewerList = ({ interviewers, value, onChange }) => {
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className='interviewers__list'>
+        {interviewers && interviewers.map((interviewer) => (
+          <InterviewerListItem
+            key={interviewer.id}
+            name={interviewer.name}
+            avatar={interviewer.avatar}
+            selected={interviewer.id === value}
+            setInterviewer={(event) => onChange(interviewer.id)}
+          />
+        ))}
+      </ul>
+    </section>
+  )
 }
+
+export default InterviewerList;
