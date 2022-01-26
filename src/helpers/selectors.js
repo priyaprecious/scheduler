e479ad099c2495224ecd439acd01e3a3302abf91
priyaprecious <priyanka.datti@gmail.com>
2022-01-26 18:27:57 -0500

    implemented getInterview and getInterviewForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,13 +1,13 @@
 export function getAppointmentsForDay(state, day) {
-    // filter from list of days to find the specific day
+    
     const selectedDay = state.days.filter(oneDay => oneDay.name === day);
     
-    // if filtered array contains no data, return the empty array
+    
     if (selectedDay.length === 0) {
       return selectedDay;
     }
   
-    // map through each appointment for the day, return the appointment object where its id is equal to the appointment id in the day object
+    
     const listOfAppointments = selectedDay[0].appointments.map(id => {
       for (const appointment in state.appointments) {
         if (id === state.appointments[appointment].id) {
@@ -20,3 +20,43 @@ export function getAppointmentsForDay(state, day) {
     return listOfAppointments;
   };
   
+  export function getInterview(state, interview) {
+    const selectedInterview = {};
+    
+    if (!interview) {
+      return null;
+    }
+  
+    
+    Object.keys(state.interviewers).forEach(key => {
+      if (state.interviewers[key].id === interview.interviewer) {
+        selectedInterview.student = interview.student;
+        selectedInterview.interviewer = state.interviewers[key];
+      }
+    });
+  
+    return selectedInterview;
+  };
+  
+
+  export function getInterviewersForDay(state, day) {
+    
+    const selectedDay = state.days.filter(oneDay => oneDay.name === day);
+    
+    
+    if (selectedDay.length < 1) {
+      return selectedDay;
+    }
+  
+    
+    const listOfInterviewers = selectedDay[0].interviewers.map(id => {
+      for (const interviewer in state.interviewers) {
+        if (id === state.interviewers[interviewer].id) {
+          return state.interviewers[interviewer];
+        }
+      }
+      return null;
+    });
+  
+    return listOfInterviewers;
+  };
