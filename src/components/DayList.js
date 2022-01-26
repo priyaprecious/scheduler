e22ac2cedacc11f4f20bdafa55f4d6785e1ef926
priyaprecious <priyanka.datti@gmail.com>
2022-01-25 23:07:14 -0500

    daylist building

---
+++ src/components/DayList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import DayListItem from './DayListItem';
+
+export default function DayList({ days, day, setDay }) {
+    return (
+      <ul>
+        {days.map(d => <DayListItem key={d.id} name={d.name} spots={d.spots} selected={d.name === day} setDay={setDay} />)}
+      </ul>
+    );
+  }
+  
