24742d2153c9868c96a0f791e49110cedb3c6425
priyaprecious <priyanka.datti@gmail.com>
2022-01-26 18:20:08 -0500

    managing state

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,20 +1,20 @@
-import React from 'react';
-
-import DayListItem from './DayListItem';
+import React from "react";
+import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
+  const days = props.days.map(day => {
     return (
-        <ul>
-          {props.days.map((day) => (
-            <DayListItem
-              key={day.id}
-              name={day.name}
-              spots={day.spots}
-              selected={day.name === props.value}
-              setDay={props.onChange}
-            />
-          ))}
-        </ul>
-      );
-  }
+      <DayListItem 
+        key={day.id} 
+        name={day.name} 
+        spots={day.spots} 
+        setDay={props.onChange} 
+        selected={day.name === props.value} 
+      />
+    ); 
+  });
   
+  return (
+    <ul>{days}</ul>
+  );
+}
