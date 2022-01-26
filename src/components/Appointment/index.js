9c98e4b27d9f9f9430476c258aa8d6a12579badf
priyaprecious <priyanka.datti@gmail.com>
2022-01-26 15:13:05 -0500

    displaying appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './styles.scss';
 
-export default function Appointment({ time }) {
+import Header from 'components/Appointment/Header';
+import Show from 'components/Appointment/Show';
+import Empty from 'components/Appointment/Empty';
+
+export default function Appointment({ props }) {
     return (
         <article className="appointment">
-          {time ? `Appointment at ${time}` : 'No Appointments'}
+            <Header time={props.time} />
+            {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
         </article>
-      );
+    );
 }
