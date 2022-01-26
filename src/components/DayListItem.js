1f3f4f16cf218be133dc4d83fdfe5fcf81c7afc2
priyaprecious <priyanka.datti@gmail.com>
2022-01-25 22:59:39 -0500

    daylistitem testing

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,6 +5,16 @@ import classNames from 'classnames';
 
 export default function DayListItem(props) {
 
+    const formatSpots = (n) => {
+        if (n === 0) {
+          return 'no spots remaining';
+        } else if (n === 1) {
+          return '1 spot remaining';
+        } else {
+          return `${n} spots remaining`;
+        }
+      };
+
     const dayClass = classNames(
         "day-list__item",
         { "day-list__item--selected": props.selected },
@@ -13,7 +23,7 @@ export default function DayListItem(props) {
     return (
         <li className={dayClass} selected={props.selected} onClick={() => props.setDay(props.name)}>
             <h2 className="text--regular">{props.name}</h2>
-            <h3 className="text--light">{props.spots} spots remaining</h3>
+            <h3 className="text--light">{formatSpots(props.spots)}</h3>
         </li>
     );
 }
