e9650045659b1971c8566a968cacd504c61fe29e
priyaprecious <priyanka.datti@gmail.com>
2022-01-26 00:35:31 -0500

    refactoring interviewer components

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,16 +4,16 @@ import 'components/InterviewerListItem.scss';
 
 
 export default function InterviewerListItem({ name, avatar, selected, setInterviewer }) {
-  const liClasses = classNames('interviewers__item', { 'interviewers__item--selected': selected });
-  return (
-    <li className={liClasses} onClick={setInterviewer}>
-      <img
-        className="interviewers__item-image"
-        src={avatar}
-        alt={name}
-      />
-      {selected && name}
-    </li>
-
-  );
+    const liClasses = classNames('interviewers__item', { 'interviewers__item--selected': selected });
+    return (
+      <li className={liClasses} onClick={setInterviewer}>
+        <img
+          className="interviewers__item-image"
+          src={avatar}
+          alt={name}
+        />
+        {selected && name}
+      </li>
+  
+    );
 }
