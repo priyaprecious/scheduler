16571c48cd428fa0a3c0835ddb92579fc8fe2454
priyaprecious <priyanka.datti@gmail.com>
2022-01-27 21:15:56 -0500

    added comments

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,106 +2,106 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function useApplicationData() {
-  
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  })
+// all states into one object
+    const [state, setState] = useState({
+        day: "Monday",
+        days: [],
+        appointments: {},
+        interviewers: {}
+    })
 
-  
-  const setDay = day => setState({ ...state, day });
+// update day state
+    const setDay = day => setState({ ...state, day });
 
-  
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    }).catch(error => console.log(`Error: ${error.message}`));
-  }, []);
+// get data from database and set state with the data received
+    useEffect(() => {
+        Promise.all([
+            axios.get('/api/days'),
+            axios.get('/api/appointments'),
+            axios.get('/api/interviewers')
+        ]).then((all) => {
+            setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+        }).catch(error => console.log(`Error: ${error.message}`));
+    }, []);
 
-  
-  function bookInterview(id, interview) {
-    
-  
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    
-  
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    
-  
-    const dayBooked = state.days.filter(day => day.name === state.day)
-    const dayIndex = state.days.findIndex(day => day.name === state.day)
 
-  
-    const updateSpot = {
-      ...dayBooked[0],
-      spots: (state.appointments[id].interview ? dayBooked[0].spots : dayBooked[0].spots - 1)
+    function bookInterview(id, interview) {
+
+// update appointment based on the id
+        const appointment = {
+            ...state.appointments[id],
+            interview: { ...interview }
+        };
+
+// add new appointment to the existing list of appointments
+        const appointments = {
+            ...state.appointments,
+            [id]: appointment
+        };
+
+
+        const dayBooked = state.days.filter(day => day.name === state.day)
+        const dayIndex = state.days.findIndex(day => day.name === state.day)
+
+// contains new interview
+        const updateSpot = {
+            ...dayBooked[0],
+            spots: (state.appointments[id].interview ? dayBooked[0].spots : dayBooked[0].spots - 1)
+        }
+
+// update the previous state of the particular day
+        const days = [
+            ...state.days.slice(0, dayIndex),
+            updateSpot,
+            ...state.days.slice(dayIndex + 1)
+        ]
+
+
+        return axios.put(`/api/appointments/${id}`, { interview })
+            .then(() => {
+                setState(prev => ({ ...prev, appointments, days }));
+            });
     }
 
-  
-    const days = [
-      ...state.days.slice(0, dayIndex),
-      updateSpot,
-      ...state.days.slice(dayIndex + 1)
-    ]
+// to cancel the interview
+    function cancelInterview(id) {
 
-  
-    return axios.put(`/api/appointments/${id}`, { interview })
-      .then(() => {        
-        setState(prev => ({...prev, appointments, days}));
-      });
-  }   
-  
-  
-  function cancelInterview(id) {
-    
-  
-    const cancelledAppointment = {
-      ...state.appointments[id],
-      interview: null
-    };
 
-  
-    const appointments = {
-      ...state.appointments,
-      [id]: cancelledAppointment
-    };
-    
-  
-    const dayBooked = state.days.filter(day => day.name === state.day)
-    const dayIndex = state.days.findIndex(day => day.name === state.day)
+        const cancelledAppointment = {
+            ...state.appointments[id],
+            interview: null
+        };
 
-  
-    const updateSpot = {
-      ...dayBooked[0],
-      spots: (dayBooked[0].spots + 1)
+// append the cancelled appointment to the list of appointments
+        const appointments = {
+            ...state.appointments,
+            [id]: cancelledAppointment
+        };
+
+
+        const dayBooked = state.days.filter(day => day.name === state.day)
+        const dayIndex = state.days.findIndex(day => day.name === state.day)
+
+// particular day where the interview got cancelled
+        const updateSpot = {
+            ...dayBooked[0],
+            spots: (dayBooked[0].spots + 1)
+        }
+
+// update the previous state of the particular day
+        const days = [
+            ...state.days.slice(0, dayIndex),
+            updateSpot,
+            ...state.days.slice(dayIndex + 1)
+        ]
+
+
+        return axios.delete(`/api/appointments/${id}`)
+            .then(() => {
+                setState(prev => ({ ...prev, appointments, days }));
+            });
     }
 
-  
-    const days = [
-      ...state.days.slice(0, dayIndex),
-      updateSpot,
-      ...state.days.slice(dayIndex + 1)
-    ]
 
-  
-    return axios.delete(`/api/appointments/${id}`)
-      .then(() => {        
-        setState(prev => ({...prev, appointments, days}));
-      });
-  }
-
-  
-  return { state, setDay, bookInterview, cancelInterview };
+    return { state, setDay, bookInterview, cancelInterview };
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,32 +1,32 @@
 import { useState } from 'react';
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([initial]);
-  
-  
-  const transition = (change, replace = false) => {
-    setMode(change);
+    const [mode, setMode] = useState(initial);
+    const [history, setHistory] = useState([initial]);
 
-    if (!replace) {
-      setHistory(history => [...history, change]);
-    }
-  }
-  
-  
-  const back = () => {
-    if (history.length > 1) {
-      const previous = history.slice(0, -1);
-      
-      setHistory(previous);
-      setMode(previous[previous.length - 1]);
-    }
-  }
 
-  
-  return {
-    mode,
-    transition,
-    back
-  };
+    const transition = (change, replace = false) => {
+        setMode(change);
+
+        if (!replace) {
+            setHistory(history => [...history, change]);
+        }
+    }
+
+
+    const back = () => {
+        if (history.length > 1) {
+            const previous = history.slice(0, -1);
+
+            setHistory(previous);
+            setMode(previous[previous.length - 1]);
+        }
+    }
+
+
+    return {
+        mode,
+        transition,
+        back
+    };
 }
