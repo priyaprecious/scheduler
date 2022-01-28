f3218c84c49ccedf00f6e497630d8fac329dc8ad
priyaprecious <priyanka.datti@gmail.com>
2022-01-27 21:10:25 -0500

    added comment

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,18 +4,19 @@ import "components/Button.scss";
 import classNames from "classnames";
 
 export default function Button(props) {
+   // different classes for different type of button
    const buttonClass = classNames("button", {
       "button--confirm": props.confirm,
       "button--danger": props.danger
-    });
-  
-    return (
+   });
+
+   return (
       <button
-        className={buttonClass}
-        onClick={props.onClick}
-        disabled={props.disabled}
+         className={buttonClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
       >
-        {props.children}
+         {props.children}
       </button>
-    );
+   );
 }
